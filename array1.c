d26069458dd164da695bb64518b2ff4cb979a20c
deynikhilkumar95-beep <deynikhilkumar95@gmail.com>
2026-09-22 08:03:33 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,20 @@
+// WAP to copy the element of one array into another and display the copy array.
+#include <stdio.h>
+int main() {
+    int a[100], b[100], n, i;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++) {
+        b[i] = a[i];
+    }
+    printf("Copy array is:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", b[i]);
+    }
+    return 0;
+}
+
