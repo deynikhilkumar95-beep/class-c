40ef635aa96cefd4c938916f433bb87c3f0405cd
deynikhilkumar95-beep <deynikhilkumar95@gmail.com>
2026-09-22 08:19:41 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,15 @@
+// WAP to find the sumof all element of an array 
+#include <stdio.h>
+int main() {
+    int a[100], n, i, sum = 0;
+    printf("Enter the number of array: ");
+    scanf("%d", &n);
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+    printf("Sum of all elements = %d", sum);
+    return 0;
+}
+
